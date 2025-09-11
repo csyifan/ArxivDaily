f9de14f9d1aa63b4b3218cd07771c64ff788cca1
yifan <82603067+csyifan@users.noreply.github.com>
2025-09-11 10:28:51 +0400

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ with open("README.md", "r") as f:
     # if last_update_date == current_date:
         # sys.exit("Already updated today!")
 
-keywords = ["Video Understanding", "Prompt Learning", "Biology Large Language Models"] # TODO add more keywords
+keywords = ["Video Understanding", "Agent", "Prompt Learning", "Medical Models", "LLM Safety"] # TODO add more keywords
 
 max_result = 25 # maximum query results from arXiv API for each keyword
 issues_result = 15 # maximum papers to be included in the issue
